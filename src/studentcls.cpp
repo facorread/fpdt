8baed86e49bfda417a1c8d9f9b74f6dde7a097b0
Fabio A. Correa <facorread@gmail.com>
2016-09-24 00:14:11 -0400

    Indentation

--- src/studentcls.cpp
+++ src/studentcls.cpp
@@ -36,9 +36,9 @@ namespace fpdt {
 	const listOfFilesCls extractXML(const std::string& filename) {
 		cleanExtractedFiles();
 		std::system(std::string(
-									"{ set -o xtrace; unzip '" + filename + "' word/document.xml 'word/media/*.emf' xl/sharedStrings.xml 'xl/worksheets/sheet*.xml'"
-												"; [ \"$(echo word/media/*.emf)\" != \"word/media/*.emf\" ] && { libreoffice --headless --convert-to pdf --outdir word/media word/media/*.emf; for file in word/media/*.pdf; do pdftotext \"${file}\"; done; } } >& fpdt.log"
-												).c_str());
+		"{ set -o xtrace; unzip '" + filename + "' word/document.xml 'word/media/*.emf' xl/sharedStrings.xml 'xl/worksheets/sheet*.xml'"
+		"; [ \"$(echo word/media/*.emf)\" != \"word/media/*.emf\" ] && { libreoffice --headless --convert-to pdf --outdir word/media word/media/*.emf; for file in word/media/*.pdf; do pdftotext \"${file}\"; done; } } >& fpdt.log"
+		).c_str());
 		return listOfFiles("word/document.xml word/media/*.txt xl/sharedStrings.xml xl/worksheets/sheet*.xml");
 	}
 
