6e0ad2020e676375e126da95bd594442d67c7f0d
Fabio A. Correa <facorread@gmail.com>
2016-11-05 08:31:11 -0400

    Too much overhead

--- src/extractxml.sh
+++ src/extractxml.sh
@@ -2,8 +2,6 @@
 
 {
 	set -o xtrace
-	[ -d fpdf_pdf ] || mkdir -v fpdf_pdf
-	libreoffice --headless --convert-to pdf --outdir fpdf_pdf "$@"
 	unzip "$@" word/document.xml 'word/media/*.emf' xl/sharedStrings.xml 'xl/worksheets/sheet*.xml'
 	[ \"$(echo word/media/*.emf)\" != \"word/media/*.emf\" ] && {
 		libreoffice --headless --convert-to pdf --outdir word/media word/media/*.emf
